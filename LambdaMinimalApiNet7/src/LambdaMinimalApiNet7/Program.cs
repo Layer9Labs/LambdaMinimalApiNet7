2fa20f4742945e1b5d98d137d33b7d61aae4efc7
Orgl9l <github@layer9labs.com>
2023-07-05 17:45:03 -0700

    Remove code generated Serialization back to basic sample

--- LambdaMinimalApiNet7/src/LambdaMinimalApiNet7/Program.cs
+++ LambdaMinimalApiNet7/src/LambdaMinimalApiNet7/Program.cs
@@ -7,20 +7,22 @@ builder.Services.AddControllers();
 
 // Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
 // package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
-builder.Logging.AddLambdaLogger();
+
+//builder.Logging.AddLambdaLogger();
 builder.Services
-    .Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
-    {
-        options.SerializerOptions.AddContext<LambdaFunctionJsonSerializerContext>();
-        options.SerializerOptions.PropertyNameCaseInsensitive = false;
-        options.SerializerOptions.PropertyNamingPolicy = null;
-        options.SerializerOptions.WriteIndented = true;
-    })
-    .AddHttpLogging(logging =>
-    {
-    })
+    // .Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+    // {
+    //     options.SerializerOptions.AddContext<LambdaFunctionJsonSerializerContext>();
+    //     options.SerializerOptions.PropertyNameCaseInsensitive = false;
+    //     options.SerializerOptions.PropertyNamingPolicy = null;
+    //     options.SerializerOptions.WriteIndented = true;
+    // })
+    // .AddHttpLogging(logging =>
+    // {
+    // })
     .AddAWSLambdaHosting(LambdaEventSource.RestApi)
-    .AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+    //.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions())
+    ;
 
 var app = builder.Build();
 
